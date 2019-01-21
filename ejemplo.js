7244bcfd6d796d1484767ba13c510d41b661465b
Jorge <jlegazpegutierrez@gmail.com>
2019-01-21 13:46:34 +0100

    propiedad imagen creada en objeto

---
+++ ejemplo.js
@@ -0,0 +1,166 @@
+window.onload = function() {
+  // get canvas related references
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+  var BB = canvas.getBoundingClientRect();
+  var offsetX = BB.left;
+  var offsetY = BB.top;
+  var WIDTH = canvas.width;
+  var HEIGHT = canvas.height;
+  var background = new Image();
+
+  // drag related variables
+  var dragok = false;
+  var startX;
+  var startY;
+
+  // an array of objects that define different rectangles
+  var rects = [];
+  rects.push({
+    x: 250 - 15,
+    y: 350 - 15,
+    width: 30,
+    height: 30,
+    fill: "#444444",
+    image: "images/brain.png",
+    isDragging: false
+  });
+  rects.push({
+    x: 250 - 15,
+    y: 400 - 15,
+    width: 30,
+    height: 30,
+    fill: "#ff550d",
+    image: "images/heart.png",
+    isDragging: false
+  });
+  // rects.push({
+  //   x: 250 - 15,
+  //   y: 450 - 15,
+  //   width: 30,
+  //   height: 30,
+  //   fill: "#800080",
+  //   isDragging: false
+  // });
+  // rects.push({
+  //   x: 250 - 15,
+  //   y: 500 - 15,
+  //   width: 30,
+  //   height: 30,
+  //   fill: "#0c64e8",
+  //   isDragging: false
+  // });
+
+  // listen for mouse events
+  canvas.onmousedown = myDown;
+  canvas.onmouseup = myUp;
+  canvas.onmousemove = myMove;
+
+  // call to draw the scene
+  draw();
+
+  // draw a single rect
+  function rect(x, y, w, h) {
+    ctx.beginPath();
+    ctx.rect(x, y, w, h);
+    ctx.closePath();
+    ctx.fill();
+  }
+
+  // clear the canvas
+  function clear() {
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+  }
+
+  // redraw the scene
+  function draw() {
+    clear();
+    background.src = "images/escenario.png";
+    ctx.drawImage(background, 0, 0, 1200, 600);
+    // redraw each rect in the rects[] array
+    //ctx.fillRect(0, 0, 120, 120);
+    // redraw each rect in the rects[] array
+    // for (var i = 0; i < rects.length; i++) {
+    //   var r = rects[i];
+    //   var img = new Image();
+    //   img.scr = r.image;
+    //   //ctx.fillStyle = r.fill;
+    //   ctx.drawImage(img, r.x, r.y, r.width, r.height);
+    // }
+  }
+
+  // handle mousedown events
+  function myDown(e) {
+    // tell the browser we're handling this mouse event
+    e.preventDefault();
+    e.stopPropagation();
+
+    // get the current mouse position
+    var mx = parseInt(e.clientX - offsetX);
+    var my = parseInt(e.clientY - offsetY);
+
+    // test each rect to see if mouse is inside
+    dragok = false;
+    for (var i = 0; i < rects.length; i++) {
+      var r = rects[i];
+      if (mx > r.x && mx < r.x + r.width && my > r.y && my < r.y + r.height) {
+        // if yes, set that rects isDragging=true
+        dragok = true;
+        r.isDragging = true;
+      }
+    }
+    // save the current mouse position
+    startX = mx;
+    startY = my;
+  }
+
+  // handle mouseup events
+  function myUp(e) {
+    // tell the browser we're handling this mouse event
+    e.preventDefault();
+    e.stopPropagation();
+
+    // clear all the dragging flags
+    dragok = false;
+    for (var i = 0; i < rects.length; i++) {
+      rects[i].isDragging = false;
+    }
+  }
+
+  // handle mouse moves
+  function myMove(e) {
+    // if we're dragging anything...
+    if (dragok) {
+      // tell the browser we're handling this mouse event
+      e.preventDefault();
+      e.stopPropagation();
+
+      // get the current mouse position
+      var mx = parseInt(e.clientX - offsetX);
+      var my = parseInt(e.clientY - offsetY);
+
+      // calculate the distance the mouse has moved
+      // since the last mousemove
+      var dx = mx - startX;
+      var dy = my - startY;
+
+      // move each rect that isDragging
+      // by the distance the mouse has moved
+      // since the last mousemove
+      for (var i = 0; i < rects.length; i++) {
+        var r = rects[i];
+        if (r.isDragging) {
+          r.x += dx;
+          r.y += dy;
+        }
+      }
+
+      // redraw the scene with the new rect positions
+      draw();
+
+      // reset the starting mouse position for the next mousemove
+      startX = mx;
+      startY = my;
+    }
+  }
+}; // end $(function(){});
--- script.js
+++ script.js
@@ -1,42 +1,44 @@
 window.onload = function() {
   var canvas;
   var ctx;
-  var x = 75;
-  var y = 50;
+  var xBone = 200;
+  var yBone = 300;
+  var xBrain = 150;
+  var yBrain = 250;
   var WIDTH = 1200;
   var HEIGHT = 600;
   var dragok = false;
-  var background = new Image();
 
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
 
-  background.src = "images/escenario.png";
-  ctx.drawImage(background, 0, 0, 1200, 600);
-
+  var background = new Image();
   var bone = new Image();
+  var brain = new Image();
 
   function draw() {
-    clear();
-    ctx.fillStyle = "#FAF7F8";
-    rect(0, 0, WIDTH, HEIGHT);
-    ctx.fillStyle = "#444444";
-    rect(x - 15, y - 15, 30, 30);
-    // background.src = "images/escenario.png";
-    // ctx.drawImage(background, 0, 0, 1200, 600);
-    ctx.drawImage(bone, 10, 10, 50, 50);
+    // clear();
+    // ctx.fillStyle = "#FAF7F8";
+    // rect(0, 0, WIDTH, HEIGHT);
+    // ctx.fillStyle = "#444444";
+    // rect(x - 15, y - 15, 30, 30);
+    background.src = "images/escenario.png";
+    ctx.drawImage(background, 0, 0, 1200, 600);
+    ctx.drawImage(bone, xBone, yBone, 30, 30);
     bone.src = "images/bone.png";
+    ctx.drawImage(brain, xBrain, yBrain, 30, 30);
+    brain.src = "images/brain.png";
   }
 
-  function rect(x, y, w, h) {
-    ctx.beginPath();
-    ctx.rect(x, y, w, h);
-    ctx.closePath();
-    ctx.fill();
-  }
-  function clear() {
-    ctx.clearRect(0, 0, WIDTH, HEIGHT);
-  }
+  // function rect(x, y, w, h) {
+  //   ctx.beginPath();
+  //   ctx.rect(x, y, w, h);
+  //   ctx.closePath();
+  //   ctx.fill();
+  // }
+  // function clear() {
+  //   ctx.clearRect(0, 0, WIDTH, HEIGHT);
+  // }
 
   function init() {
     canvas = document.getElementById("canvas");
@@ -46,20 +48,35 @@ window.onload = function() {
 
   function myMove(e) {
     if (dragok) {
-      x = e.pageX - canvas.offsetLeft;
-      y = e.pageY - canvas.offsetTop;
+      xBone = e.pageX - canvas.offsetLeft;
+      yBone = e.pageY - canvas.offsetTop;
+    }
+    if (dragok) {
+      xBrain = e.pageX - canvas.offsetLeft;
+      yBrain = e.pageY - canvas.offsetTop;
     }
   }
 
   function myDown(e) {
     if (
-      e.pageX < x + 15 + canvas.offsetLeft &&
-      e.pageX > x - 15 + canvas.offsetLeft &&
-      e.pageY < y + 15 + canvas.offsetTop &&
-      e.pageY > y - 15 + canvas.offsetTop
+      e.pageX < xBone + 15 + canvas.offsetLeft &&
+      e.pageX > xBone - 15 + canvas.offsetLeft &&
+      e.pageY < yBone + 15 + canvas.offsetTop &&
+      e.pageY > yBone - 15 + canvas.offsetTop
     ) {
-      x = e.pageX - canvas.offsetLeft;
-      y = e.pageY - canvas.offsetTop;
+      xBone = e.pageX - canvas.offsetLeft;
+      yBone = e.pageY - canvas.offsetTop;
+      dragok = true;
+      canvas.onmousemove = myMove;
+    }
+    if (
+      e.pageX < xBrain + 15 + canvas.offsetLeft &&
+      e.pageX > xBrain - 15 + canvas.offsetLeft &&
+      e.pageY < yBrain + 15 + canvas.offsetTop &&
+      e.pageY > yBrain - 15 + canvas.offsetTop
+    ) {
+      xBrain = e.pageX - canvas.offsetLeft;
+      yBrain = e.pageY - canvas.offsetTop;
       dragok = true;
       canvas.onmousemove = myMove;
     }
